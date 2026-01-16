dc67e89a2027ba70cb4ec3318e9642752ec84afd
p20113 <p20113@sw.hs.kr>
2026-01-16 14:18:28 +0900

    Update gshow.py

--- pages/gshow.py
+++ pages/gshow.py
@@ -11,9 +11,10 @@ keyword = st.selectbox("Select keyword:", (klist))
 df_keyword = df[df['키워드'] == keyword]
 st.dataframe(df_keyword)
 # 날짜별 값만 추출
-df_plot = df_keyword.drop(columns=['키워드']).T  # 전치
-df_plot.columns = ['Value']  # 컬럼 이름 변경
-df_plot.index = pd.to_datetime(df_plot.index)  # 인덱스를 날짜로 변환
+
+#df_plot = df_keyword.drop(columns=['키워드']).T  # 전치
+#df_plot.columns = ['Value']  # 컬럼 이름 변경
+#df_plot.index = pd.to_datetime(df_plot.index)  # 인덱스를 날짜로 변환
 plt.figure(figsize=(10,5))
 plt.plot(list(df.columns[1:]),df.loc[keyword].tolist(), marker='o')
 plt.title(f'{keyword} 키워드 11월 데이터')

bdf1596a057275bce1b13a7470c497ab97cfcd5d
p20113 <p20113@sw.hs.kr>
2026-01-16 15:55:05 +0900

    Update gshow.py

--- pages/gshow.py
+++ pages/gshow.py
@@ -12,12 +12,16 @@ df_keyword = df[df['키워드'] == keyword]
 st.dataframe(df_keyword)
 st.write(df)
 # 날짜별 값만 추출
-
+ylist=list()
+keyl=df['키워드']
+targetind=keyl.index(keyword)
+for dictkey in df[1:]:
+  ylist.append(df[dictkey][targetind])
 #df_plot = df_keyword.drop(columns=['키워드']).T  # 전치
 #df_plot.columns = ['Value']  # 컬럼 이름 변경
 #df_plot.index = pd.to_datetime(df_plot.index)  # 인덱스를 날짜로 변환
 plt.figure(figsize=(10,5))
-plt.plot(list(df.columns[1:]),df.loc[keyword].tolist()[1:], marker='o')
+plt.plot(list(df.columns[1:]),ylist, marker='o')
 plt.title(f'{keyword} 키워드 11월 데이터')
 plt.xlabel('Date')
 plt.ylabel('Value')

3a17a37272d64a74609f83f92a1a641b87498c7e
p20113 <p20113@sw.hs.kr>
2026-01-16 13:33:04 +0900

    Update show.py

--- pages/show.py
+++ pages/show.py
@@ -12,6 +12,7 @@ st.title("News data")
 
 # 키워드 선택
 keyword = st.selectbox("Select keyword:", klist)
+'''
 df_keyword = df['날짜',keyward] 
 #df_keyword = df[df['날짜'] == keyword]
 st.dataframe(df_keyword)
@@ -20,10 +21,11 @@ df_plot = df[['날짜', keyword]].copy()
 df_plot.set_index('날짜', inplace=True)
 df_plot.index = pd.to_datetime(df_plot.index)
 df_plot.columns = ['Value']
-
+'''
 # 그래프 그리기
 plt.figure(figsize=(10,5))
-plt.plot(df_plot.index, df_plot['Value'], marker='o')
+#plt.plot(df_plot.index, df_plot['Value'], marker='o')
+plt.plot(df['날짜'],df[keyword],marker='o')
 plt.title(f'{keyword} 키워드 11월 데이터')
 plt.xlabel('Date')
 plt.ylabel('Value')
